af896f24d2eaf21895084af8f7545f4657d56eb4
Vlad-Fliahin <vvvf29012002@gmail.com>
2021-11-09 21:31:09 +0200

    Secret key cross out

--- MovieKnight/settings.py
+++ MovieKnight/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w$--fp9^6ip3%^gx$48r5#0j2z&1)7ujvp$yt=uq+4mcl#&m$8'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
